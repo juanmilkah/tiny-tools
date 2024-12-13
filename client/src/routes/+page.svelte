94449f5c02d9a3ee40e461c14ce435cca90dfe12
Juan Milkah <juanmilkah@gmail.com>
2024-12-13 11:45:55 +0300

    switch from next to svelte

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="bg-blue-700">Tiny Tools</h1>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,18 +1,12 @@
-import type { Config } from "tailwindcss";
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [typography]
 } satisfies Config;
